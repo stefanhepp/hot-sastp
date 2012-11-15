5999e6348362aadf695f61fe951841e662011796
ioan-dragan <ioan.dumitru.dragan@gmail.com>
2012-11-15 18:03:18 +0100

    Comment style example

--- src/solver.cpp
+++ src/solver.cpp
@@ -11,13 +11,16 @@ void usage(const char *filename) {
 
 }
 
+/**
+ * example comment 
+ */
+
 int main(int argc, char* argv[]) {
     
     if (argc != 2) {
 	usage(argv[0]);
 	return 1;
     }
-
     // --- Handle arguments  ---
 
     string fin(argv[1]);
