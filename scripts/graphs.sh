e96c8122a81bac4712f384fccb1eefc7925c9ef8
Stefan Hepp <stefan@stefant.org>
2012-12-03 19:17:50 +0100

    added random grasp step results

--- scripts/graphs.sh
+++ scripts/graphs.sh
@@ -24,15 +24,15 @@ $SOLVER -a 2 -s 1 -n 2 -cp $INSTANCEDIR/sastp1000.prob $OUTPUTDIR/sastp1000.sol 
 
 $SOLVER -a 3 -s 2 -n 0 -cp $INSTANCEDIR/sastp1000.prob $OUTPUTDIR/sastp1000.sol > $REPORTDIR/graphs/grasp.ls.oneopt.best.csv
 $SOLVER -a 3 -s 1 -n 0 -cp $INSTANCEDIR/sastp1000.prob $OUTPUTDIR/sastp1000.sol > $REPORTDIR/graphs/grasp.ls.oneopt.next.csv
-# $SOLVER -a 3 -s 0 -n 0 -cp $INSTANCEDIR/sastp1000.prob $OUTPUTDIR/sastp1000.sol > $REPORTDIR/graphs/grasp.ls.oneopt.rand.csv
+$SOLVER -a 3 -s 0 -n 0 -cp $INSTANCEDIR/sastp1000.prob $OUTPUTDIR/sastp1000.sol > $REPORTDIR/graphs/grasp.ls.oneopt.rand.csv
 
 $SOLVER -a 3 -s 2 -n 1 -cp $INSTANCEDIR/sastp1000.prob $OUTPUTDIR/sastp1000.sol > $REPORTDIR/graphs/grasp.ls.edgeopt.best.csv
 $SOLVER -a 3 -s 1 -n 1 -cp $INSTANCEDIR/sastp1000.prob $OUTPUTDIR/sastp1000.sol > $REPORTDIR/graphs/grasp.ls.edgeopt.next.csv
-# $SOLVER -a 3 -s 0 -n 1 -cp $INSTANCEDIR/sastp1000.prob $OUTPUTDIR/sastp1000.sol > $REPORTDIR/graphs/grasp.ls.edgeopt.rand.csv
+$SOLVER -a 3 -s 0 -n 1 -cp $INSTANCEDIR/sastp1000.prob $OUTPUTDIR/sastp1000.sol > $REPORTDIR/graphs/grasp.ls.edgeopt.rand.csv
 
 $SOLVER -a 3 -s 2 -n 2 -cp $INSTANCEDIR/sastp1000.prob $OUTPUTDIR/sastp1000.sol > $REPORTDIR/graphs/grasp.ls.methodopt.best.csv
 $SOLVER -a 3 -s 1 -n 2 -cp $INSTANCEDIR/sastp1000.prob $OUTPUTDIR/sastp1000.sol > $REPORTDIR/graphs/grasp.ls.methodopt.next.csv
-# $SOLVER -a 3 -s 0 -n 2 -cp $INSTANCEDIR/sastp1000.prob $OUTPUTDIR/sastp1000.sol > $REPORTDIR/graphs/grasp.ls.methodopt.rand.csv
+$SOLVER -a 3 -s 0 -n 2 -cp $INSTANCEDIR/sastp1000.prob $OUTPUTDIR/sastp1000.sol > $REPORTDIR/graphs/grasp.ls.methodopt.rand.csv
 
 $SOLVER -a 4 -s 2 -n 2 -cp $INSTANCEDIR/sastp1000.prob $OUTPUTDIR/sastp1000.sol > $REPORTDIR/graphs/grasp.vnd.best.csv
 $SOLVER -a 4 -s 1 -n 2 -cp $INSTANCEDIR/sastp1000.prob $OUTPUTDIR/sastp1000.sol > $REPORTDIR/graphs/grasp.vnd.next.csv
