be184a940e98a40bb32dd2c47ca655675822fedf
Stefan Hepp <stefan@stefant.org>
2012-12-04 12:35:27 +0100

    Added results of new neighborhood

--- scripts/solveall.sh
+++ scripts/solveall.sh
@@ -5,5 +5,5 @@ basedir=`dirname $0`
 . $basedir/sastp.cfg
 
 for i in 10 20 50 100 200 500 1000; do
-    $SOLVER $@ $INSTANCEDIR/sastp_$i.prob $OUTPUTDIR/sastp_$i.sol
+    $SOLVER $@ $INSTANCEDIR/sastp$i.prob $OUTPUTDIR/sastp$i.sol
 done
